5c91ef79e16633eb4e96551be689106fcfc2da0c
elisabichiappini <elisabichiappini@gmail.com>
2024-02-20 01:50:30 +0100

    feat: check index with search

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -12,7 +12,14 @@ class ProjectController extends Controller
         request()->validate([
             'key'=>['nullable', 'string', 'min:5'],
         ]);
-        $projects = Project::with('technologies', 'type')->paginate(9);
+        // dd(request()->key); http://127.0.0.1:8001/api/projects?page=1&key=ciaos
+        // dd(request()->key);
+        if(request()->key) {
+            $projects = Project::where('title', 'LIKE', '%' . request()->key . '%')->paginate(9);
+        } else {
+            $projects = Project::with('technologies', 'type')->paginate(9);
+        }
+
         return response()->json([
             'success' => true,
             'results' => $projects
