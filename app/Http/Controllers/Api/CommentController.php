e0d5fe80c9b250962c229661a8b8b9d2f2819052
elisabichiappini <elisabichiappini@gmail.com>
2024-03-03 22:29:20 +0100

    fix: comments not function

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -10,18 +10,19 @@ class CommentController extends Controller
 {
     public function store(Request $request) {
 
-        $data = request()->validate([
+        $data = $request->validate([
 
             'author' => ['nullable', 'string', 'max:1'],
             'content' => ['string', 'max:400', 'required'],
             'project_id' => ['required', 'exists:projects,id', 'integer'],
+            
         ]);
 
         // $new_comment = new Comment();
         // $new_comment->fill($data);
         // $new_comment->save();
-
-        
+       
         return $request->all();
+ 
     }
 }
